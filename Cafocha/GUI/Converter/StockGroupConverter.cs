37fa8d3c44c2907503f68bac11d124bfd36c7677
VỸ PHẠM HÙNG <15521037@ms.uit.edu.vn>
2019-01-12 18:42:51 +0700

    Add stock type group

--- Cafocha/GUI/Converter/StockGroupConverter.cs
+++ Cafocha/GUI/Converter/StockGroupConverter.cs
@@ -9,14 +9,16 @@ using Cafocha.BusinessContext.WarehouseWorkspace;
 namespace Cafocha.GUI.Converter
 {
 
-    [ValueConversion(typeof(int), typeof(string))]
+    [ValueConversion(typeof(string), typeof(string))]
     public class StockGroupConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             return (from types in WarehouseModule.StockTypes
-                where types.StId.Equals((int) value)
-                select types).FirstOrDefault();
+                where types.StId.Equals(value)
+                select types).FirstOrDefault()
+                ?.Name;
+
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
