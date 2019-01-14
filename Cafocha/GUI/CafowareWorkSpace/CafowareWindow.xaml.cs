b1af40c5cd493e13bd9e9d83e33a0ff256a8dd2d
VỸ PHẠM HÙNG <15521037@ms.uit.edu.vn>
2019-01-14 19:04:21 +0700

    Fix salarynote

--- Cafocha/GUI/CafowareWorkSpace/CafowareWindow.xaml.cs
+++ Cafocha/GUI/CafowareWorkSpace/CafowareWindow.xaml.cs
@@ -111,8 +111,7 @@ namespace Cafocha.GUI.CafowareWorkSpace
 
         private void bntLogout_Click(object sender, RoutedEventArgs e)
         {
-            Application.Current.Properties["AdLogin"] = null;
-            Application.Current.Properties["EmpLogin"] = null;
+            _businessModuleLocator.EmployeeModule.Emploglist.Clear();
             _loginWindow = new LoginWindow();
             Close();
             _loginWindow.Show();
